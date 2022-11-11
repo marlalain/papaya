cd916a5af7c86262c591b94d1da0c506caa22fb2
Max Power <alex@alexaa.dev>
2022-11-11 19:59:29 -0300

    feat: basic classes that render html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,84 @@
-console.log("Hello world");
+const { http } = Melon;
+const app = http.app({
+	name: "papaya",
+	host: "localhost",
+	port: 5000,
+	enableHttps: false,
+});
+
+type HtmlTag = 'body' | 'div' | 'p' | 'h1';
+
+class Element {
+	private readonly _tag: string;
+	private _attributes: Record<string, string>;
+	private readonly _selfClosing: boolean;
+	private _children: string[];
+
+	constructor(tag: HtmlTag, options?: { selfClosing: boolean }) {
+		this._tag = tag;
+
+		if (options) this._selfClosing = options.selfClosing;
+	}
+
+	addAttribute(attribute: Record<string, string>) {
+		this._attributes = {
+			...this._attributes,
+			...attribute,
+		};
+	}
+
+	children(txt: TemplateStringsArray | string[]) {
+		this._children = [...txt];
+	}
+
+	render() {
+		if (this._selfClosing) {
+			return `<${this._tag} />`;
+		} else {
+			return `<${this._tag}>${this._children.join('')}</${this._tag}>`;
+		}
+	}
+
+	renderAsChildren() {
+		return [this.render()];
+	}
+}
+
+class Page {
+	private _head: string = "<head></head>";
+	private _title?: string;
+	private _body: Element = new Element("body");
+
+	head(str: TemplateStringsArray) {
+		const text = str[0];
+
+		if (this._title) {
+			this._head = `<head>${text}<title>${this._title}</title></head>`;
+		} else {
+			this._head = `<head>${text}</head>`;
+		}
+	}
+
+	title(str: string) {
+		this._title = str;
+	}
+
+	body(str: TemplateStringsArray | string[]) {
+		this._body.children(str);
+	}
+
+	render() {
+		return `<html lang="en">${this._head}${this._body.render()}</html>`;
+	}
+}
+
+const page = new Page();
+page.title("My Melon Wiki");
+
+const helloTitle = new Element('h1');
+helloTitle.children`My Melon Wiki!`;
+page.body(helloTitle.renderAsChildren());
+
+app.get("/", async () => http.static(page.render(), 'text/html'));
+
+app.run();
